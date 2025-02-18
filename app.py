9608b570f4ecde1f7f58149d168a52cbf4fb3cbc
cahcharm <126590861+CahCharm@users.noreply.github.com>
2025-02-18 09:55:53 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-print('cora food\n')
+import os
 
-print('1. Cadastrar restaurante') 
-print('2. Listar restaurante') 
-print('3. Ativar restaurante') 
-print('4. Sair')
+def exibir_nome_do_programa():
+ print('cora food\n')
+
+def exibir_opcoes():
+ print('1. Cadastrar restaurante') 
+ print('2. Listar restaurante') 
+ print('3. Ativar restaurante') 
+ print('4. Sair')
 
 opcao_escolhida = int(input('Escolha uma opção: '))
 print(f'Você escolheu a opção {opcao_escolhida}')
+
+def finalizar_app():
+     os.system('cls')
+     print('Finalizar o app')
 
 if opcao_escolhida == 1:
     print('Cadastrar restaurante')
@@ -15,4 +23,11 @@ elif opcao_escolhida ==2:
 elif opcao_escolhida ==3:
      print('Ativar restaurante') 
 else:
-     print ('Encerrando o programa')
+     finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+
+if __name__ == '__main__':
+     main()
